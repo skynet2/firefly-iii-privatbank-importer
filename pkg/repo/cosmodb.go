49beefe46040ef8e144b0377ab6837c0df3e5abc
Stas Dm <iqpirat@gmail.com>
2024-01-29 11:04:01 +0100

    feat: base cosmodb

--- pkg/repo/cosmodb.go
+++ pkg/repo/cosmodb.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+	"github.com/cockroachdb/errors"
 
 	"github.com/skynet2/firefly-iii-privatbank-importer/pkg/database"
 )
@@ -17,6 +19,56 @@ type Cosmo struct {
 	cl *azcosmos.DatabaseClient
 }
 
+func NewCosmo(
+	cl *azcosmos.Client,
+	dbName string,
+) (*Cosmo, error) {
+	_, err := cl.CreateDatabase(context.Background(), azcosmos.DatabaseProperties{
+		ID: dbName,
+	}, &azcosmos.CreateDatabaseOptions{})
+
+	c := &Cosmo{}
+
+	if realErr := c.ignoreDuplicateErr(err); realErr != nil {
+		return nil, realErr
+	}
+
+	db, err := cl.NewDatabase(dbName)
+	if err != nil {
+		return nil, err
+	}
+	c.cl = db
+
+	if err = c.setupContainers(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Cosmo) setupContainers() error {
+	_, err := c.cl.CreateContainer(context.Background(), azcosmos.ContainerProperties{
+		ID: messagesContainer,
+		PartitionKeyDefinition: azcosmos.PartitionKeyDefinition{
+			Paths: []string{"/isProcessed"},
+		},
+	}, &azcosmos.CreateContainerOptions{})
+
+	return c.ignoreDuplicateErr(err)
+}
+
+func (c *Cosmo) ignoreDuplicateErr(err error) error {
+	if err == nil {
+		return nil
+	}
+	var azureErr *azcore.ResponseError
+	if errors.As(err, &azureErr) && azureErr.StatusCode == 409 {
+		return nil
+	}
+
+	return err
+}
+
 func (c *Cosmo) AddMessage(ctx context.Context, message database.Message) error {
 	partitionKey := azcosmos.NewPartitionKeyBool(message.IsProcessed)
 
@@ -27,7 +79,7 @@ func (c *Cosmo) AddMessage(ctx context.Context, message database.Message) error 
 
 	container, err := c.getMessageContainer()
 
-	_, err = container.UpsertItem(ctx, partitionKey, bytes, nil)
+	_, err = container.CreateItem(ctx, partitionKey, bytes, nil)
 	if err != nil {
 		return err
 	}
@@ -46,7 +98,7 @@ func (c *Cosmo) GetLatestMessages(ctx context.Context) ([]*database.Message, err
 
 	partitionKey := azcosmos.NewPartitionKeyBool(false)
 
-	query := "SELECT * FROM messages where is_processed = false order by created_at desc"
+	query := "SELECT * FROM c where c.isProcessed = false order by c.createdAt desc"
 	pager := container.NewQueryItemsPager(query, partitionKey, nil)
 
 	var items []*database.Message
@@ -80,10 +132,4 @@ func (c *Cosmo) Clear(ctx context.Context) error {
 	_, err = container.Delete(ctx, &azcosmos.DeleteContainerOptions{})
 
 	return err
-}
-
-func NewCosmo(cl *azcosmos.DatabaseClient) *Cosmo {
-	return &Cosmo{
-		cl: cl,
-	}
 }
